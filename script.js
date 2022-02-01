1974a64f5a55d3bd2ef383df9d85e291d641c6d0
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-01-31 21:37:08 -0800

    Enable the form to update and display the array

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let library = [
-    {author: "Author Lastname1", title: "Book1", numPages: 240, isRead: true},
-    {author: "Author Lastname2", title: "Book2", numPages: 124, isRead: false},
-    {author: "Author Lastname3", title: "Book3", numPages: 224, isRead: false},
-    {author: "Author Lastname4", title: "Book4", numPages: 345, isRead: true},
+    new Book("author100", "title100", 434, false),
+    new Book("author1", "title1", 25, true),
+    new Book("author2", "title2", 32, true),
+    new Book("author3", "title3", 55, false),
+    new Book("author4", "title4", 233, true),
 ];
+
+displayEachBook();
 
 const newBookButton = document.querySelector('#new-book');
 const hiddenForm = document.querySelector('#hidden-form');
@@ -11,13 +14,23 @@ newBookButton.addEventListener('click', () => {
     hiddenForm.classList.toggle('hidden');
 });
 
-displayEachBook();
+const submitButton = document.querySelector('#submitButton');
+submitButton.addEventListener('click', () => {
+    let authorText = document.querySelector('#authorInput').value;
+    let titleText = document.querySelector('#titleInput').value;
+    let numPagesText = document.querySelector('#numberPages').value;
+    let isReadCheck = document.querySelector('#isRead').checked;
+    let newBook = new Book(authorText, titleText, numPagesText, isReadCheck);
+    library.push(newBook);
+    refreshTable();
+    displayEachBook();
+});
 
 function Book(author, title, numPages, isRead) {
-    author,
-    title,
-    numPages,
-    isRead
+    this.author = author,
+    this.title = title,
+    this.numPages = numPages,
+    this.isRead = isRead
 }
 
 function addBookToLibrary(bookTitle) {
@@ -44,4 +57,26 @@ function displayEachBook() {
         newRow.appendChild(numPagesCell);
         newRow.appendChild(isReadCell);
     });
+}
+
+function refreshTable() {
+    const bookTable = document.querySelector('#books');
+    while (bookTable.firstChild) {
+        bookTable.removeChild(bookTable.firstChild);
+    }
+    const tr = document.createElement('tr');
+    const thAuthor = document.createElement('th');
+    thAuthor.textContent = "Author";
+    const thTitle = document.createElement('th');
+    thTitle.textContent = "Title";
+    const thPage = document.createElement('th');
+    thPage.textContent = "Page Count";
+    const thRead = document.createElement('th');
+    thRead.textContent = "Read";
+
+    bookTable.appendChild(tr);
+    tr.appendChild(thAuthor);
+    tr.appendChild(thTitle);
+    tr.appendChild(thPage);
+    tr.appendChild(thRead);
 }
